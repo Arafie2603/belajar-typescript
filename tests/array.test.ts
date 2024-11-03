0eb8a445c39474829b69cbd1154430acd0afa33d
Arafie2603 <arrafiesetiawan@gmail.com>
2024-11-03 14:30:21 +0700

    array dan tupple readonly ts

--- tests/array.test.ts
+++ tests/array.test.ts
@@ -8,10 +8,20 @@ describe('array', function () {
     });
 
     it('should support readonly array', function () {
-        const hobbies: ReadonlyArray<String> = ['membaca', 'menulis'];
+        const hobbies: ReadonlyArray<string> = ['membaca', 'menulis'];
         console.info(hobbies)
         console.info(hobbies[0])
 
         // hobbies[0] = 'main game'; / error
+    });
+
+    it('should support tuple', function () {
+        const person: readonly [string, string, number] = ['ara', 'sil', 21];
+        console.info(person);
+        console.info(person);
+        console.info(person[0]);
+        console.info(person[1]);
+
+        // person[0] = 'budi'; /error
     });
 });
