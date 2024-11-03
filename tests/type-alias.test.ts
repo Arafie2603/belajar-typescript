787939ea016d8c6a3023d4bef7ae296178be40a6
Arafie2603 <arrafiesetiawan@gmail.com>
2024-11-03 15:47:43 +0700

    unit test type alias

---
+++ tests/type-alias.test.ts
@@ -0,0 +1,21 @@
+import {Category, Product} from "../src/type-alias";
+describe('Type alias', function () {
+    it('should support in Typescript', function () {
+        const category : Category = {
+            id: "1",
+            name: "handphone",
+        };
+
+        const product: Product = {
+            id: "1",
+            name: "Samsung s20",
+            price: 200000,
+            category: category,
+        };
+
+        // product.descript = "Test" / error
+
+        console.info(category);
+        console.info(product);
+    });
+});
