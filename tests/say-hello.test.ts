8e09799b8b2966d0f8f3280108c1a8f841e781bf
Arafie2603 <arrafiesetiawan@gmail.com>
2024-11-04 19:27:22 +0700

    delete

--- tests/say-hello.test.ts
+++
@@ -1,6 +0,0 @@
-import { sayHello } from "../src/say-hello";
-describe('sayHello', function () {
-    it('should return hello ara', function () {
-        expect(sayHello('ara')).toBe('Hello ara');
-    });
-});
