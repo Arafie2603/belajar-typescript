2df59ede7a18e5ed145d59f5168b9d7439bebb1d
Arafie2603 <arrafiesetiawan@gmail.com>
2024-11-04 19:20:28 +0700

    test

---
+++ dasar-typescript/tests/tipe-data.test.ts
@@ -0,0 +1,11 @@
+describe('Data Type', function () {
+    it('should must declare', function () {
+        let name: String = "Ara";
+        let balance: Number = 100000;
+        let isVip: Boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+    });
+});
